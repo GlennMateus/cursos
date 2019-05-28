d2eb3face73d6944c8aac09efaa503b717b7738e
Glenn S.V. Mateus <glenn.mateus@bonnjur.com.br>
2019-05-28 14:16:28 -0300

    Começando com curso ReactJS

--- curso_es6/src/main.js
+++ curso_es6/src/main.js
@@ -1,8 +1,12 @@
+import api from './api';
+
 class App {
   constructor() {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
+    this.inputEl = document.querySelector('input[name=repository]');
 
     this.registerHandlers();
   }
@@ -10,17 +14,74 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  setLoading(loading = true) {
+    if (loading) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
+  async addRepository(event) {
     event.preventDefault();
+    const repoInput = this.inputEl.value;
+    if (repoInput.length === 0) return;
+    this.setLoading();
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url }
+      } = response.data;
 
-    this.repositories.push({
-      name: 'rocketseat.com.br',
-      description: 'Tire sua ideia do papel e dê vida à sua startup',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'https://github.com/Rocketseat'
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
+
+      this.render();
+      this.inputEl.value = '';
+    } catch (error) {
+      alert('repositório inexistente');
+    }
+
+    this.setLoading(false);
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titlleEl = document.createElement('strong');
+      titlleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titlleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
     });
-
-    console.log(this.repositories);
   }
 }
 
