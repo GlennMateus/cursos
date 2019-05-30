ee282a56bb49d9dfda21bcbd019268ff759ca4f3
Glenn Mateus <glennmateus@gmail.com>
2019-05-29 22:17:21 -0300

    Reconfigurado projeto localmente e adicionado o CORS; Utilizar comando: npm install cors

--- Node JS/node-api/server.js
+++ Node JS/node-api/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-// Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodepai', {
+// // Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useNewUrlParser: true
 });
 requireDir('./src/models');
 
-// Rotas
+// // Rotas
 app.use('/api', require('./src/routes'));
 
 app.listen(3001);
