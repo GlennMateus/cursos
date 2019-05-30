5e84d298153274284f210e5cd7e0220d4439cca3
Glenn S.V. Mateus <glenn.mateus@bonnjur.com.br>
2019-05-29 21:22:35 -0300

    ProductsController

---
+++ Node JS/README.md
@@ -0,0 +1,12 @@
+### O que é o Express?
+
+    É um framework que auxiliará o desenvolvedor a lidar com rotas e views
+
+### O que dá pra capturar a partir do 'req'?
+
+    Parâmetros, corpo da requisição, cabeçalho da requisição, usuário que fez a requisição,
+    autenticação (se está ou não autenticado), IP, etc.
+
+### O que é o 'res'?
+
+    'res' é a resposta que será dada para o usuário.
---
+++ Node JS/node-api/src/controllers/ProductController.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Product = mongoose.model('Product');
+
+module.exports = {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const products = await Product.paginate({}, { page, limit: 10 });
+
+    return res.json(products);
+  },
+
+  async detail(req, res) {
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product);
+  },
+
+  async create(req, res) {
+    const product = await Product.create(req.body);
+
+    return res.json(product);
+  },
+
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      //Retorna o dado atualizado. Sem isso ele irá retornar o dado antigo
+      new: true
+    });
+
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+
+    return res.send('Produto removido com sucesso');
+  }
+};
